4211361adc05c8164b2d346b314677a2efc78acd
Yuito Murase <yuito@acupof.coffee>
2024-04-15 12:50:02 +0900

    Refine init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -18,11 +18,12 @@
 (setq scroll-conservatively 10000)
 (setq scroll-margin 0)
 (setq scroll-preserve-screen-position t)
- 
+
 ;; view
 (show-paren-mode t)
 (column-number-mode t)
 (global-display-line-numbers-mode)
+(setq-default display-line-numbers-width 3)
 (tab-bar-mode 1)
 (if window-system
     (tool-bar-mode 0)
@@ -49,10 +50,11 @@
 (package-initialize)
 (if package-archive-contents
     (package-refresh-contents t)
-    (package-refresh-contents))
+  (package-refresh-contents))
 
 (setq my-favorite-packages
-      '(auctex
+      '(
+        auctex
         ddskk
         magit
         markdown-mode
@@ -66,7 +68,7 @@
         proof-general
         tuareg
         solarized-theme
-        highlight-indent-guides))
+        indent-guide))
 
 ;; possibly useful packages
 '(dtrt-indent
@@ -76,8 +78,8 @@
   (unless (package-installed-p package)
     (package-install package)))
 
-;;; highlight-indent-guide-mode
-(add-hook 'prog-mode-hook 'highlight-indent-guides-mode)
+;;; indent-guide
+(indent-guide-global-mode)
 
 ;;; xclip
 (unless window-system (xclip-mode 1))
@@ -135,7 +137,7 @@
 ;; which is annoying when backspacing over a '('
 (defun override-slime-repl-bindings-with-paredit ()
   (define-key slime-repl-mode-map
-    (read-kbd-macro paredit-backward-delete-key) nil))
+              (read-kbd-macro paredit-backward-delete-key) nil))
 (add-hook 'slime-repl-mode-hook 'override-slime-repl-bindings-with-paredit)
 
 ;; Beluga mode
@@ -144,18 +146,18 @@
 
 (when window-system
   (set-face-attribute 'default nil :family "Source Han Code JP" :height 120)
-  ; 全角かな設定
+                                        ; 全角かな設定
   (set-fontset-font (frame-parameter nil 'font)
                     'japanese-jisx0208
                     (font-spec :family "Source Han Code JP" :size 14))
-  ; 半角ｶﾅ設定
+                                        ; 半角ｶﾅ設定
   (set-fontset-font (frame-parameter nil 'font)
                     'katakana-jisx0201
                     (font-spec :family "Source Han Code JP" :size 14))
-  ; ずれ確認用
-  ; 0123456789012345678901234567890123456789
-  ; ｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵ
-  ; あいうえおあいうえおあいうえおあいうえお
+                                        ; ずれ確認用
+                                        ; 0123456789012345678901234567890123456789
+                                        ; ｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵｱｲｳｴｵ
+                                        ; あいうえおあいうえおあいうえおあいうえお
   )
 
 (custom-set-variables
@@ -168,7 +170,7 @@
    '("3e200d49451ec4b8baa068c989e7fba2a97646091fd555eca0ee5a1386d56077" "285d1bf306091644fb49993341e0ad8bafe57130d9981b680c1dbd974475c5c7" "00445e6f15d31e9afaa23ed0d765850e9cd5e929be5e8e63b114a3346236c44c" default))
  '(highlight-indent-guides-method 'character)
  '(package-selected-packages
-   '(highlight-indent-guides solarized-theme tuareg proof-general dracula-theme color-theme-sanityinc-tomorrow xclip undo-tree smooth-scroll slime paredit markdown-mode magit ddskk auctex))
+   '(indent-guide highlight-indent-guides solarized-theme tuareg proof-general dracula-theme color-theme-sanityinc-tomorrow xclip undo-tree smooth-scroll slime paredit markdown-mode magit ddskk auctex))
  '(warning-suppress-types '(((unlock-file)) ((unlock-file)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
